36ebea98b6dd273390010996de02e66cb230bbca
1lent <brodiecuff@outlook.com>
2025-01-18 22:26:34 +0800

    customisation :shock:

--- rick.py
+++ rick.py
@@ -1,11 +1,15 @@
 import webbrowser
 import time
 
-timer = 0
-
-webbrowser.open("https://www.youtube.com/watch?v=_-2dIuV34cs")
-time.sleep(60)
-while timer < 100:
-    time.sleep(1)
-    webbrowser.open("https://www.youtube.com/watch?v=AbtpZvC8aT4")
-    timer = timer + 1
+def miku_sucks():
+    timer = 0
+    webbrowser.open("https://www.youtube.com/watch?v=_-2dIuV34cs")
+    time.sleep(5)
+    while timer < 250000:
+        time.sleep(1)
+        if (timer % 2) == 0:
+            webbrowser.open_new("https://www.youtube.com/watch?v=9Deg7VrpHbM")
+        else:
+            webbrowser.open_new("https://www.youtube.com/watch?v=tvBQFon3yas")
+        timer += 1
+miku_sucks()
